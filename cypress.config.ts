7cb85f5c0320361649c3424076c731605a382631
TStefan <taraipanstefan97@gmail.com>
2024-09-29 14:29:33 +0300

    add mochawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://angular.realworld.how",
     excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples"],
     watchForFileChanges: false,
     numTestsKeptInMemory: 3,
     specPattern: "cypress/integration/*.ts",
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 });
